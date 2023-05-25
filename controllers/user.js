93fe602a32a1bbacd8cf57d99698665d8e84c7d2
st-audrey <st_audrey@outlook.fr>
2023-05-25 10:51:17 +0200

    create user login

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,31 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({ message: "Paire id/mdp invalide" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res.status(401).json({ message: "Paire id/mdp invalide" });
+            } else {
+              res.status(200).json({ userId: user._id, token: "TOKEN" });
+            }
+          })
+          .catch((error) => {
+            res.status(500).json({ error });
+          });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
 exports.signup = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -20,5 +45,3 @@ exports.signup = (req, res, next) => {
     })
     .catch((error) => res.status(500).json({ error }));
 };
-
-exports.login = (req, res, next) => {};
