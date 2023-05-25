90a796f426e8024888df6e758651e3a61b98740a
st-audrey <st_audrey@outlook.fr>
2023-05-25 10:23:24 +0200

    create user signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,24 @@
-exports.signup = (req, res, next) => {};
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() =>
+          res
+            .status(201)
+            .json({ message: "L'utilisateur a bien été enregistré" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
 exports.login = (req, res, next) => {};
