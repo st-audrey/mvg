2d86c1fc153974de9c162a7136a1545fd338d0e5
st-audrey <st_audrey@outlook.fr>
2023-05-26 13:51:05 +0200

    add get book by id

--- controllers/book.js
+++ controllers/book.js
@@ -10,7 +10,11 @@ exports.getAllBooks = (req, res, next) => {
     );
 };
 
-exports.getBookById = (req, res, next) => {};
+exports.getBookById = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
+};
 
 exports.getBookWithBestRating = (req, res, next) => {};
 
